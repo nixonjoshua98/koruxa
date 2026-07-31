6209fb1a60fec25dbe5df392cd5c714669ebcc4a
Joshua Nixon <nixonjoshua98@gmail.com>
2026-07-31 20:16:50 +0100

    Updated channel id

--- src/SRC.DiscordBot/KoruxaConstant.cs
+++ src/SRC.DiscordBot/KoruxaConstant.cs
@@ -6,7 +6,7 @@ internal static class KoruxaConstant
 {
     public static readonly TimeSpan AttackReminderDelay = TimeSpan.FromHours(4);
 
-    public const ulong KoruxaChannelId = 1514011813283500063;
+    public const ulong KoruxaChannelId = 1532818564472373358;
     
     public static readonly TimeZoneInfo LocalTimeZone = OperatingSystem.IsWindows()
         ? TimeZoneInfo.FindSystemTimeZoneById("Central Standard Time")
